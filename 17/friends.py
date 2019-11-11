233ecf7a91dde2107cf6f0466f5e43b55f6b6442
Mike Hambe <michael.hambe@gmail.com>
2019-11-11 20:48:49 +1100

    adding solution

--- 17/friends.py
+++ 17/friends.py
@@ -5,3 +5,13 @@ def friends_teams(friends, team_size=2, order_does_matter=False):
         return set(list(it.combinations(friends, team_size)))
     else:
         return list(it.permutations(friends, team_size))
+
+# import itertools
+
+
+# def friends_teams(friends, team_size=2, order_does_matter=False):
+#     if order_does_matter:
+#         func = itertools.permutations
+#     else:
+#         func = itertools.combinations
+#     return func(friends, team_size)
